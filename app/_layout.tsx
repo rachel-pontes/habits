38a223259aa72807bc59e7841d394f33af92e3dc
Rachel Pontes <rachel.pontes@dali.dartmouth.edu>
2025-05-19 14:50:06 -0400

    added notification

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,21 @@
 // app/_layout.tsx
+import React, { useEffect } from 'react';
 import { Stack } from 'expo-router';
+import { requestNotificationPermission, scheduleDailyReminder } from '../utils/notifications';
+
 
 export default function Layout() {
+  useEffect(() => {
+    async function setupNotifications() {
+      const hasPermission = await requestNotificationPermission();
+      if (hasPermission) {
+        // Schedule the reminder for 9 PM (21:00)
+        await scheduleDailyReminder(14, 48);
+      }
+    }
+    setupNotifications();
+  }, []);
+
   return (
       <Stack />
   );
